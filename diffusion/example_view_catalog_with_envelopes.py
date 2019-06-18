5b0fbc035e73c1109112a3d17367625446eb6bfa
cerinunn <cerinunn@users.noreply.github.com>
2019-06-18 10:11:03 -0700

    Update example_view_catalog_with_envelopes.py

--- diffusion/example_view_catalog_with_envelopes.py
+++ diffusion/example_view_catalog_with_envelopes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-View the catalog
+View the catalog with envelopes 
 
 :copyright:
     The PDART Development Team & Ceri Nunn
@@ -16,37 +16,32 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from future.builtins import *  # NOQA
 from pdart.diffusion.view_catalog_with_envelopes import view_catalog_with_envelopes
-# from builtins import input
-# from obspy import read_events, read_inventory
-# from obspy.core import read, Stream
-# from obspy.core.util.misc import limit_numpy_fft_cache
-# from obspy.signal.filter import envelope
-# from future.utils import native_str
-# import os.path
-# import numpy as np
-# from matplotlib.dates import date2num
-# import math
-# 
-# 
-# import matplotlib
-# matplotlib.use('TkAgg')
-# import matplotlib.pyplot as plt
-# 
-# # from matplotlib import rcParams
-# # rcParams.update({'figure.autolayout': True})
-# 
 
 
 if __name__ == "__main__":
 
-    top_level_processed_dir = '/Users/nunn/lunar_data/PDART_PROCESSED'
-    inv_name = "/Users/nunn/lunar_data/IRIS_dataless_seed/XA.1969-1977.xml"
+    top_level_processed_dir = '/Users/nunn/Google Drive/for_Britt/PDART_PROCESSED'
+    inv_name = "/Users/nunn/Google Drive/for_Britt/inventory/XA.1969-1977.xml"
     pre_filt_main = [0.2,0.3,0.9,1.3]
     pre_filt_env = [[0.1,0.25,0.75,1],[0.5,0.75,1.25,1.5],[1,1.25,1.75,2.25]]
 
+
+    print('''The Lognonne_2003.xml catalog contains measured longitude/latitudes 
+        for the artificial impacts.
+        The catolog contains estimates for most other events.''')
     view_catalog_with_envelopes(top_level_processed_dir,
-      '../lunar_proposal/catalogs/S12_shallow.xml',inv_name,
+      '/Users/nunn/lunar_data/lunar_catalogs/output/Lognonne_2003/Lognonne_2003.xml',inv_name,
      dir_type='processed_dir',pre_filt_main=pre_filt_main,
-     pre_filt_env=pre_filt_env,output='ACC',smooth_periods=10,
+     pre_filt_env=pre_filt_env,output='ACC',smooth_periods=30,
      plot_type='with_seismogram')
+
+    # original catalog
+    # view_catalog_with_envelopes(top_level_processed_dir,
+    #   '/Users/nunn/Google Drive/for_Britt/catalogs/S12_shallow.xml',inv_name,
+    #  dir_type='processed_dir',pre_filt_main=pre_filt_main,
+    #  pre_filt_env=pre_filt_env,output='ACC',smooth_periods=30,
+    #  plot_type='with_seismogram')
+
+
+
 
