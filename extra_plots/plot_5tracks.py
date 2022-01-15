b649562d23035cc17751da773ba13150f14ed68c
cerinunn <ceri.nunn@gmail.com>
2022-01-14 16:32:55 -0800

    increased font sizes

--- extra_plots/plot_5tracks.py
+++ extra_plots/plot_5tracks.py
@@ -31,7 +31,7 @@ def five_tracks():
     end_time = start_time + timedelta(minutes=(2+12))
 
     stream = stream_from_directory_new(
-      top_level_dir='/Users/cnunn/lunar_data/PDART_CONTINUOUS_MAIN_TAPES',
+      top_level_dir='/Users/cnunn/lunar_data/PDART',
       start_time=start_time,
       stations=stations,
       channels=channels,
@@ -48,7 +48,7 @@ def five_tracks():
 
     ########
 
-    fig = plt.figure(figsize=(15, 4))
+    fig = plt.figure(figsize=(10, 2.6))
     gs = gridspec.GridSpec(5, 1, hspace=0.001)
 
     ax0 = plt.subplot(gs[0])
@@ -66,60 +66,60 @@ def five_tracks():
     ax0.set_xlim(-2*60, 12*60)
     ax0.set_xticks([0,180,360,540,720])
     ax0.set_xticks(np.arange(-120,720,60), minor=True)
-    ax0.set_title('5 Tracks', fontsize=20)
+    ax0.set_title('5 Tracks', fontsize=12)
 
     ax0.set_yticks([470,510,550])
     # ax0.set_yticks(np.arange(460,580,20), minor=True)
     # ax0.set_ylim(510-80, 510+80)
-    ax0.set_ylabel('DU', fontsize=14)
-    ax0.annotate(xy=(0.01,0.9), text=onset.strftime("%Y-%m-%d %H:%M:%S"),
-      fontsize=13, horizontalalignment="left", verticalalignment="top",
+    ax0.set_ylabel('DU', fontsize=12)
+    ax0.annotate(onset.strftime("%Y-%m-%d %H:%M:%S") ,xy=(0.01,0.9),
+      fontsize=12, horizontalalignment="left", verticalalignment="top",
       xycoords="axes fraction")
 
     ax1 = plt.subplot(gs[1], sharex=ax0)
     trace_MH1 = stream.select(channel='MH1')[0]
-    ax1.plot(times_to_seconds(trace_MH1.times()), trace_MH1.data, color='k')
+    ax1.plot(times_to_seconds(trace_MH1.times()), trace_MH1.data, color='k', linewidth=1)
 
     yticks = ax1.set_yticks([430,510,590])
     # yticks[0].label1.set_visible(False)
     # yticks[-1].label1.set_visible(False)
     # ax1.set_ylim(510-140, 510+140)
     # ax1.set_yticks(np.arange(400, 1000, 200))
-    ax1.set_ylabel('DU', fontsize=14)
+    ax1.set_ylabel('DU', fontsize=12)
 
     ax2 = plt.subplot(gs[2], sharex=ax0)
     trace_MH2 = stream.select(channel='MH2')[0]
-    ax2.plot(times_to_seconds(trace_MH2.times()), trace_MH2.data, color='k')
+    ax2.plot(times_to_seconds(trace_MH2.times()), trace_MH2.data, color='k', linewidth=1)
 
     # ax2.set_ylim(510-60, 510+60)
     ax2.set_ylim(280, 1000)
     ax2.set_yticks([310,510,710])
-    ax2.set_ylabel('DU', fontsize=14)
+    ax2.set_ylabel('DU', fontsize=12)
 
     ax3 = plt.subplot(gs[3], sharex=ax0)
     trace_SHZ = stream.select(channel='SHZ')[0]
-    ax3.plot(times_to_seconds(trace_SHZ.times()), trace_SHZ.data, color='k')
+    ax3.plot(times_to_seconds(trace_SHZ.times()), trace_SHZ.data, color='k', linewidth=1)
 
     ax3.set_yticks([330,530,730])
     ax3.set_ylim(530-300,530+300)
-    ax3.set_ylabel('DU', fontsize=14)
+    ax3.set_ylabel('DU', fontsize=12)
 
     ax4 = plt.subplot(gs[4], sharex=ax0)
     trace_ATT = stream.select(channel='ATT')[0]
     ax4.plot(times_to_seconds(trace_ATT.times()),
-      times_to_seconds(trace_ATT.data-trace_ATT.stats.starttime.timestamp), color='k')
+      times_to_seconds(trace_ATT.data-trace_ATT.stats.starttime.timestamp), color='k', linewidth=1)
 
     ax4.set_ylim(-180, 820)
     ax4.set_yticks([0,360,720])
-    ax4.set_ylabel('s',rotation=90, fontsize=14)
+    ax4.set_ylabel('s',rotation=90, fontsize=12)
 
-    ax4.annotate(xy=(0.01,0.9), text='+{} s'.format(onset.timestamp),
-      fontsize=13, horizontalalignment="left", verticalalignment="top",
+    ax4.annotate('+{} s'.format(onset.timestamp), xy=(0.01,0.9), 
+      fontsize=12, horizontalalignment="left", verticalalignment="top",
       xycoords="axes fraction")
-    ax4.annotate(xy=(0.99,0.1), text=trace_ATT.stats.station,
-      fontsize=16, horizontalalignment="right", verticalalignment="bottom",
+    ax4.annotate(trace_ATT.stats.station, xy=(0.99,0.1), 
+      fontsize=12, horizontalalignment="right", verticalalignment="bottom",
       xycoords="axes fraction")
-    ax4.set_xlabel('Seconds after arrival time', fontsize=14)
+    ax4.set_xlabel('Seconds after arrival time', fontsize=12, labelpad=1)
 
     xticklabels = (ax0.get_xticklabels() + ax1.get_xticklabels() +
       ax2.get_xticklabels() + ax3.get_xticklabels())
@@ -138,24 +138,24 @@ def five_tracks():
     ax3.yaxis.set_label_coords(-0.04, 0.5)
     ax4.yaxis.set_label_coords(-0.04, 0.5)
 
-    ax0.annotate(xy=(1.01,0.5), text='MHZ', fontsize=16,
+    ax0.annotate('MHZ', xy=(1.01,0.5), fontsize=12,
       xycoords="axes fraction", horizontalalignment='left',
       verticalalignment='center')
-    ax1.annotate(xy=(1.01,0.5), text='MH1', fontsize=16,
+    ax1.annotate('MH1', xy=(1.01,0.5), fontsize=12,
       xycoords="axes fraction", horizontalalignment='left',
       verticalalignment='center')
-    ax2.annotate(xy=(1.01,0.5), text='MH2', fontsize=16,
+    ax2.annotate('MH2', xy=(1.01,0.5), fontsize=12,
       xycoords="axes fraction", horizontalalignment='left',
       verticalalignment='center')
-    ax3.annotate(xy=(1.01,0.5), text='SHZ', fontsize=16,
+    ax3.annotate('SHZ', xy=(1.01,0.5), fontsize=12,
       xycoords="axes fraction", horizontalalignment='left',
       verticalalignment='center')
-    ax4.annotate(xy=(1.01,0.5), text='ATT', fontsize=16,
+    ax4.annotate('ATT', xy=(1.01,0.5), fontsize=12,
       xycoords="axes fraction", horizontalalignment='left',
       verticalalignment='center')
 
-    plt.subplots_adjust(left=0.06, right=0.95, top=0.9, bottom=0.12)
-    plt.savefig('5tracks_incl_SHZ_XXXX.png')
+    plt.subplots_adjust(left=0.06, right=0.95, top=0.9, bottom=0.16)
+    plt.savefig('../extra_plots_output/5tracks_incl_SHZ_XXXX.png')
     plt.show()
 
 
@@ -170,7 +170,7 @@ def five_tracks_original():
     end_time = start_time + timedelta(minutes=15)
 
     stream = stream_from_directory_new(
-      top_level_dir='/Users/cnunn/lunar_data/PDART_CONTINUOUS_MAIN_TAPES',
+      top_level_dir='/Users/cnunn/lunar_data/PDART',
       start_time=start_time,
       stations=stations,
       channels=channels,
@@ -178,7 +178,7 @@ def five_tracks_original():
 
     ########
 
-    fig = plt.figure(figsize=(15, 4))
+    fig = plt.figure(figsize=(10, 2.6))
     gs = gridspec.GridSpec(5, 1, hspace=0.001)
 
     ax0 = plt.subplot(gs[0])
@@ -188,13 +188,13 @@ def five_tracks_original():
     ax0.set_xlim(-2*60, 15*60)
     ax0.set_xticks(np.arange(0,15*60,6*50))
     ax0.set_xticks(np.arange(-180,15*60,60), minor=True)
-    ax0.set_title('5 Tracks', fontsize=20)
+    ax0.set_title('5 Tracks', fontsize=12)
 
     ax0.set_yticks(np.arange(480, 560, 20))
     # ax0.set_yticks(np.arange(460,580,20), minor=True)
     ax0.set_ylim(510-50, 510+50)
-    ax0.set_ylabel('DU', fontsize=14)
-    ax0.annotate(xy=(0.01,0.9), text=onset.strftime("%Y-%m-%d %H:%M:%S"),
+    ax0.set_ylabel('DU', fontsize=12)
+    ax0.annotate(onset.strftime("%Y-%m-%d %H:%M:%S"), xy=(0.01,0.9), 
       fontsize=13, horizontalalignment="left", verticalalignment="top",
       xycoords="axes fraction")
 
@@ -207,7 +207,7 @@ def five_tracks_original():
     # yticks[-1].label1.set_visible(False)
     ax1.set_ylim((200, 1000))
     ax1.set_yticks(np.arange(400, 1000, 200))
-    ax1.set_ylabel('DU', fontsize=14)
+    ax1.set_ylabel('DU', fontsize=12)
 
     ax2 = plt.subplot(gs[2], sharex=ax0)
     trace_MH2 = stream.select(channel='MH2')[0]
@@ -215,7 +215,7 @@ def five_tracks_original():
 
     ax2.set_ylim((380, 580))
     ax2.set_yticks(np.arange(430, 580, 50))
-    ax2.set_ylabel('DU', fontsize=14)
+    ax2.set_ylabel('DU', fontsize=12)
 
     ax3 = plt.subplot(gs[3], sharex=ax0)
     trace_AFR = stream.select(channel='AFR')[0]
@@ -223,7 +223,7 @@ def five_tracks_original():
 
     ax3.set_yticks(np.arange(0, 100, 45))
     ax3.set_ylim((-10, 100))
-    ax3.set_ylabel('Frame-\ncount',rotation=90, fontsize=14)
+    ax3.set_ylabel('Frame-\ncount',rotation=90, fontsize=12)
 
     ax4 = plt.subplot(gs[4], sharex=ax0)
     trace_ATT = stream.select(channel='ATT')[0]
@@ -232,12 +232,12 @@ def five_tracks_original():
 
     ax4.set_ylim((-200, 900))
     ax4.set_yticks(np.arange(-200, 800, 200))
-    ax4.set_ylabel('s',rotation=90, fontsize=14)
+    ax4.set_ylabel('s',rotation=90, fontsize=12)
 
-    ax4.annotate(xy=(0.01,0.9), text='+{} s'.format(onset.timestamp),
-      fontsize=13, horizontalalignment="left", verticalalignment="top",
+    ax4.annotate('+{} s'.format(onset.timestamp), xy=(0.01,0.9), 
+      fontsize=12, horizontalalignment="left", verticalalignment="top",
       xycoords="axes fraction")
-    ax4.set_xlabel('Seconds after arrival time', fontsize=14)
+    ax4.set_xlabel('Seconds after arrival time', fontsize=12, labelpad=1)
 
     xticklabels = (ax0.get_xticklabels() + ax1.get_xticklabels() +
       ax2.get_xticklabels() + ax3.get_xticklabels())
@@ -256,23 +256,23 @@ def five_tracks_original():
     ax3.yaxis.set_label_coords(-0.03, 0.5)
     ax4.yaxis.set_label_coords(-0.04, 0.5)
 
-    ax0.annotate(xy=(1.01,0.5), text='MHZ', fontsize=16,
+    ax0.annotate('MHZ', xy=(1.01,0.5), fontsize=12,
       xycoords="axes fraction", horizontalalignment='left',
       verticalalignment='center')
-    ax1.annotate(xy=(1.01,0.5), text='MH1', fontsize=16,
+    ax1.annotate('MH1', xy=(1.01,0.5), fontsize=12,
       xycoords="axes fraction", horizontalalignment='left',
       verticalalignment='center')
-    ax2.annotate(xy=(1.01,0.5), text='MH2', fontsize=16,
+    ax2.annotate('MH2', xy=(1.01,0.5), fontsize=12,
       xycoords="axes fraction", horizontalalignment='left',
       verticalalignment='center')
-    ax3.annotate(xy=(1.01,0.5), text='AFR', fontsize=16,
+    ax3.annotate('AFR', xy=(1.01,0.5), fontsize=12,
       xycoords="axes fraction", horizontalalignment='left',
       verticalalignment='center')
-    ax4.annotate(xy=(1.01,0.5), text='ATT', fontsize=16,
+    ax4.annotate('ATT', xy=(1.01,0.5), fontsize=12,
       xycoords="axes fraction", horizontalalignment='left',
       verticalalignment='center')
 
-    plt.subplots_adjust(left=0.06, right=0.95, top=0.9, bottom=0.12)
+    plt.subplots_adjust(left=0.06, right=0.95, top=0.9, bottom=0.16)
     plt.savefig('../extra_plots_output/5tracks_XXXX.png')
     plt.show()
 
