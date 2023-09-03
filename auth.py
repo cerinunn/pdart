0f3aeb2d9fa3831bfc0be0d71748495767099876
cerinunn <cerinunn@users.noreply.github.com>
2023-09-03 09:03:09 -0700

    Update auth.py
    The auth.py file is no longer required.

--- auth.py
+++ auth.py
@@ -1,5 +1,1 @@
-# The IRIS data are currently restricted. 
-# Request permission from ceri.nunn@jpl.nasa.gov,
-# then fill in user and auth_password locally. 
-user=''
-auth_password=''
+# The IRIS data are public. This file is no longer required.
